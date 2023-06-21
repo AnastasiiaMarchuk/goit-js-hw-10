b1b4a2d957e672d927b6c800e90371ab36fa523b
AnastasiiaMarchuk <stasijushka5@gmail.com>
2023-06-21 21:34:57 +0300

    finished

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -3,37 +3,34 @@ const API_KEY = 'live_mf57CBssdwkpCTgrPlzmgbxRI0rP2iUv5nIBiAm1pdJQ9OSHLPuMunDQOa
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
+export const notifyFailure = (errorMessage) => {
+  Notify.failure(errorMessage, {
+    timeout: 6000,
+    width: '550px',
+    borderRadius: '50px',
+    clickToClose: true,
+    position: 'center-top',
+    fontSize: '18px',
+  });
+};
+
+
 export const fetchBreeds = function (){
-    return fetch(`${BASE_URL}breeds?api-key=${API_KEY}`).
-    then(response => {
-        if(!response.ok){
-                  Notify.failure('😿Oops! Something went wrong! Try reloading the page!', {
-          timeout: 6000,
-          width: '700px',
-          borderRadius: '50px',
-          clickToClose: true,
-          position: 'center-top',
-          fontSize: '20px',   
-        })  
-        }
-        return response.json()
-    })
+  return fetch(`${BASE_URL}breeds?api-key=${API_KEY}`).
+  then(response => {
+      if(!response.ok){
+          notifyFailure('😿Oops! Something went wrong! Try reloading the page!')
+      }
+      return response.json()
+  })
 }
   
 
 export const fetchCatByBreed = function (breedId){
-
   return fetch(`${BASE_URL}images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
   .then(response => {
       if(!response.ok){
-        Notify.failure('😿Oops! Something went wrong! Try reloading the page!', {
-          timeout: 6000,
-          width: '700px',
-          borderRadius: '50px',
-          clickToClose: true,
-          position: 'center-top',
-          fontSize: '20px',   
-        });
+          notifyFailure('😿Oops! Something went wrong! Try reloading the page!')
       }
       return response.json()
   })
